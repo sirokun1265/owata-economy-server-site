447e837f0653ddc79d98a18290f1d2b57b0261bb
sirokun1265 <itoutubasa1265@gmail.com>
2026-08-22 12:17:30 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://doc-density-casting-cartridges.trycloudflare.com",
+  API_BASE_URL: "https://pointer-interviews-divisions-addition.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
