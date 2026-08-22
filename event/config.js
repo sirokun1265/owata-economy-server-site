2b12762cabc4490af79bc5166ce57af7eaea6917
sirokun1265 <itoutubasa1265@gmail.com>
2026-08-22 21:20:41 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://pointer-interviews-divisions-addition.trycloudflare.com",
+  API_BASE_URL: "https://mode-where-sample-fact.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
