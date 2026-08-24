68dd70d4425d4468cc96101e4ba2eebb9450c913
sirokun1265 <itoutubasa1265@gmail.com>
2026-08-24 14:18:02 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://mode-where-sample-fact.trycloudflare.com",
+  API_BASE_URL: "https://cayman-usgs-dare-personal.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
