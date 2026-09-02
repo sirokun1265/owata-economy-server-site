78efcf8260d1539598ac84c4b2872c4341b82274
sirokun1265 <itoutubasa1265@gmail.com>
2026-09-02 21:40:41 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://josh-mission-processed-charging.trycloudflare.com",
+  API_BASE_URL: "https://headset-isbn-variation-questions.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
