dd1c80af22f8c5dbf60678a04a8c31673823c883
sirokun1265 <itoutubasa1265@gmail.com>
2026-09-12 23:56:05 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://headset-isbn-variation-questions.trycloudflare.com",
+  API_BASE_URL: "https://awareness-sonic-sprint-drilling.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
