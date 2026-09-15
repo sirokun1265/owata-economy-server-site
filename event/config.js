81656ebf3af2e225b075dc72864f5e182aafe453
sirokun1265 <itoutubasa1265@gmail.com>
2026-09-15 20:54:47 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://anthropology-relevance-fit-exceptions.trycloudflare.com",
+  API_BASE_URL: "https://wrestling-butterfly-coordinate-meyer.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
