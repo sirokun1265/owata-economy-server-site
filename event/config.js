f414938ce8fc0e8ee0db4f22e840dedd988b3fb6
sirokun1265 <itoutubasa1265@gmail.com>
2026-09-17 07:00:20 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://wrestling-butterfly-coordinate-meyer.trycloudflare.com",
+  API_BASE_URL: "https://outstanding-footage-musician-artwork.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
