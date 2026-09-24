23023c0d8c2f792cd8c505f26710b652d10a1273
sirokun1265 <itoutubasa1265@gmail.com>
2026-09-25 08:44:48 +0900

    Update public OWATA event API endpoint

--- event/config.js
+++ event/config.js
@@ -1,4 +1,4 @@
 window.OWATA_EVENT_CONFIG = {
-  API_BASE_URL: "https://outstanding-footage-musician-artwork.trycloudflare.com",
+  API_BASE_URL: "https://rising-competitive-hypothetical-gbp.trycloudflare.com",
   POLL_INTERVAL_MS: 2500
 };
